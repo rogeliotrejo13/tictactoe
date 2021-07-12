473ceb7b5c590d7d93c3d94c72c3412dec488daa
Rogelio Trejo <rogelio.estanislao.garcia@ibm.com>
2021-07-12 17:13:07 -0500

    5th pomodoro,  win logic coded and 2nd refactor

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -3,23 +3,42 @@ const RL = READLINE.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-const PLAYER_1 = 'X';
-const PLAYER_2 = 'O';
+const PLAYER_ONE = 'X';
+const PLAYER_TWO = 'O';
 const BOARD_MAPPING = {1: [0,0], 2: [0,1], 3: [0,2], 4: [1,0], 5: [1,1], 6: [1,2], 7: [2,0], 8: [2,1], 9: [2,2]}
 const BOARD_CREATION_MESSAGE = "Game Board Creation...";
 const BOARD_CREATED_MESSAGE = "Board Created.";
-const START_GAME_MESSAGE = "The game will start with player X";
+const START_GAME_MESSAGE = "The game will start with player "+PLAYER_ONE;
 const PLAY_GAME_MESSAGE = "Please choice a number to play:";
 const PLAY_ERROR_MESSAGE = "Please type a number from 1-9 available in game board."
-const GAME_OVER_MESSAGE = "Game Over";
-const PLAYER_1_MOVEMENT_MESSAGE = "Game board movement, player "+PLAYER_1;
-const PLAYER_2_MOVEMENT_MESSAGE = "Game board movement, player "+PLAYER_2;
+const GAME_OVER_MESSAGE = "Final Board - Game Over";
+const END_GAME_MESSAGE =   ["PLAYER " + PLAYER_TWO + " WON!", "PLAYER " + PLAYER_ONE + " WON!", "GAME ENDS WITH A DRAW!"];
+const PLAYER_ONE_MOVEMENT_MESSAGE = "Game board movement, player "+PLAYER_ONE;
+const PLAYER_TWO_MOVEMENT_MESSAGE = "Game board movement, player "+PLAYER_TWO;
 const THINKING_MESSAGE = "Thinking...";
 const LOWEST_NUMBER_BOARD = 1;
 const HIGHEST_NUMBER_BOARD = 9;
 const LINE_BREAK = "\n";
-
+const ROUND_SECONDS = 0;
+const WIN = 1;
+const LOSE = 0;
+const DRAW = 2;
+const NUMBER_TO_WIN = 3;
+const DEFAULT_SYMBOL_WIN_MAPPING = '*';
+const WIN_SCENARIOS_MAPPING = [
+    [['*','*','*'],['','',''],['','','']],
+    [['','',''],['*','*','*'],['','','']],
+    [['','',''],['','',''],['*','*','*']],
+    [['*','',''],['*','',''],['*','','']],
+    [['','*',''],['','*',''],['','*','']],
+    [['','','*'],['','','*'],['','','*']],
+    [['*','',''],['','*',''],['','','*']],
+    [['','','*'],['','*',''],['*','','']]
+];
+  
+var sleep = require('sleep');
 var board = [[1,2,3],[4,5,6],[7,8,9]];
+var gamePlays = 0;
 
 function tictactoe() {
     printMessage(BOARD_CREATION_MESSAGE);
@@ -30,15 +49,19 @@ function tictactoe() {
 }
 
 function play(){
+    let gameOver = null;
     RL.question(PLAY_GAME_MESSAGE, function(number) { 
         if(!validateInputNumber(number) || !validateReplaceNumber(number)){
             printMessage(PLAY_ERROR_MESSAGE);         
             return play();
         }     
         round(number);
-        if(endGame(board)){
+        gameOver = endGame();
+        if(gameOver === LOSE || gameOver === WIN || gameOver === DRAW){
             RL.close();
             printMessage(GAME_OVER_MESSAGE);
+            printMessage(getStringBoard(board));
+            printMessage(END_GAME_MESSAGE[gameOver]);
         }else{
             return play();
         }
@@ -46,33 +69,62 @@ function play(){
 }
 
 function round(number){
-    replaceInputInBoard(number,PLAYER_1);
-    printMessage(PLAYER_1_MOVEMENT_MESSAGE);
+    replaceInputInBoard(number,PLAYER_ONE);
+    printMessage(PLAYER_ONE_MOVEMENT_MESSAGE);
     printMessage(getStringBoard(board));
-    randomPlay();      
-    printMessage(PLAYER_2_MOVEMENT_MESSAGE);
-    printMessage(getStringBoard(board));
+    if(!endGame()){
+        randomPlay();  
+        printMessage(PLAYER_TWO_MOVEMENT_MESSAGE);
+        printMessage(getStringBoard(board));
+    }    
 }
 
 function randomPlay(){
     let randomNumber = 0;
     do{
-        randomNumber = Math.floor(Math.random() * 9) + 1;
+        randomNumber = Math.floor(Math.random() * HIGHEST_NUMBER_BOARD) + 1;
         printMessage(THINKING_MESSAGE);
-    }while(!replaceInputInBoard(randomNumber,PLAYER_2));
+    }while(!replaceInputInBoard(randomNumber,PLAYER_TWO) && gamePlays < HIGHEST_NUMBER_BOARD);
+    sleep.sleep(ROUND_SECONDS);
 }
 
 function replaceInputInBoard(number,value){
     let positions = BOARD_MAPPING[number];
     if(validateReplaceNumber(number)){
         board[positions[0]][positions[1]] = value;
+        gamePlays++;
         return true;
     }
     return false;
 }
 
-function endGame(board){
+function endGame(){
+    if(gamePlays>=HIGHEST_NUMBER_BOARD){return 2;}
+    let isPlayerOneWinner = winnerMovementValidator(PLAYER_ONE);
+    let isPlayerTwoWinner = winnerMovementValidator(PLAYER_TWO);
+    if(isPlayerOneWinner){return 1;}
+    if(isPlayerTwoWinner){return 0;}
     return false;
+}
+
+function winnerMovementValidator(player){
+    let winner = false;
+    let winnerCounter = 0;
+    for(let i = 0; i < WIN_SCENARIOS_MAPPING.length; i++){
+        winnerCounter = 0;
+        for(let j = 0; j < WIN_SCENARIOS_MAPPING[i].length; j++){
+            for(let k = 0; k < WIN_SCENARIOS_MAPPING[i][j].length; k++){
+                if(WIN_SCENARIOS_MAPPING[i][j][k] === DEFAULT_SYMBOL_WIN_MAPPING && board[j][k] === player){
+                    winnerCounter ++;
+                }
+            }            
+        }
+        if(winnerCounter == NUMBER_TO_WIN){
+            winner = true;
+            break;
+        }         
+    }
+    return winner;
 }
 
 function validateReplaceNumber(number){
