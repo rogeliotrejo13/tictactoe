89dd4954c6d17351ac9639a3e4af589eee25b168
Rogelio Trejo <rogelio.estanislao.garcia@ibm.com>
2021-07-12 00:20:50 -0500

    Third Pomodoro, adding play logic

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,36 +1,65 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const playerX = 'X';
+const playerO = 'O';
+var board = [[1,2,3],[4,5,6],[7,8,9]];
+const boardPositionMapping = {1: [0,0], 2: [0,1], 3: [0,2], 4: [1,0], 5: [1,1], 6: [1,2], 7: [2,0], 8: [2,1], 9: [2,2]}
 
-var board = [['1','2','3'], ['4','5','6'], ['7','8','9']];
-function tictactoe(board) {
+function tictactoe() {
     console.log("Game Board Creation...");
     console.log(printBoard(board));    
     console.log("Board Created.");
     console.log("The game will start with player X");  
-    play(board);
+    play();
 
 }
 
-function play(board){
-    rl.question("Please choice a number to play:", function(number) {
-        console.log(`${number}`);
-        printBoard(board);
+function play(){
+    rl.question("Please choice a number to play:", function(number) { 
+        if(!validateInputNumber(number)){
+            console.log("Please type a number from 1-9 available in game board.")            
+            return play();
+        }     
+        replaceInputInBoard(number,playerX);
+        randomPlay();      
+        console.log(printBoard(board));
         if(endGame(board)){
             rl.close();
             console.log("Game Over");
+            return -1;
         }else{
-            play(board);
+            return play();
         }
     });
-    rl.close();
+    return -1;
+}
+
+function randomPlay(){
+    let randomNumber = 0;
+    do{
+        randomNumber = Math.floor(Math.random() * 9) + 1;
+        console.log("Thinking...")
+    }while(!replaceInputInBoard(randomNumber,playerO));
+}
+
+function replaceInputInBoard(number,value){
+    let positions = boardPositionMapping[number];
+    if(parseInt(board[positions[0]][positions[1]]) === parseInt(number)){
+        board[positions[0]][positions[1]] = value;
+        return true;
+    }
+    return false;
 }
 
 function endGame(board){
-    return 1;
+    return false;
+}
+
+function validateInputNumber(number){
+  return Number.isInteger(parseInt(number)) && number>0 && number<10;
 }
 
 function printBoard(board){    
@@ -38,4 +67,4 @@ function printBoard(board){
 }
 
 tictactoe(board);
-module.exports = {printBoard, endGame};
+module.exports = {printBoard, endGame, validateInputNumber};
--- tst/tictactoe.test.js
+++ tst/tictactoe.test.js
@@ -1,10 +1,10 @@
 const tictactoe = require('../src/tictactoe');
 
 test('Board Creation', () => {
-  expect(tictactoe.printBoard([['1','2','3'], ['4','5','6'], ['7','8','9']])).toBe('1,2,3\n4,5,6\n7,8,9'); 
+  expect(tictactoe.printBoard([[1,2,3],[4,5,6],[7,8,9]])).toBe('1,2,3\n4,5,6\n7,8,9'); 
 });
 
-test('WIN', () => {
+test('Win', () => {
   expect(tictactoe.endGame([['X','X','X'],['','',''],['','','']])).toBe(1);
   expect(tictactoe.endGame([['','',''],['X','X','X'],['','','']])).toBe(1);
   expect(tictactoe.endGame([['','',''],['','',''],['X','X','X']])).toBe(1);
@@ -28,4 +28,18 @@ test('Lose', () => {
 
 test('Draw', () => {
   expect(tictactoe.endGame([['X','O','X'],['O','O','X'],['X','X','O']])).toBe(2);
+});
+
+test('Validate Input Number', () => {
+  expect(tictactoe.validateInputNumber(1)).toBe(true);
+  expect(tictactoe.validateInputNumber(2)).toBe(true);
+  expect(tictactoe.validateInputNumber(3)).toBe(true);
+  expect(tictactoe.validateInputNumber(4)).toBe(true);
+  expect(tictactoe.validateInputNumber(5)).toBe(true);
+  expect(tictactoe.validateInputNumber(6)).toBe(true);
+  expect(tictactoe.validateInputNumber(7)).toBe(true);
+  expect(tictactoe.validateInputNumber(8)).toBe(true);
+  expect(tictactoe.validateInputNumber(9)).toBe(true);
+  expect(tictactoe.validateInputNumber(10)).toBe(false);
+  expect(tictactoe.validateInputNumber('')).toBe(false);
 });
