5bc02d5b81293d8eab5653c2d019bb65cf2139d9
Rogelio Trejo <rogelio.estanislao.garcia@ibm.com>
2021-07-12 18:20:18 -0500

    tictactoe deploy 7th July 2021 rebase, merge dev into master

---
+++ tst/tictactoe.test.js
@@ -0,0 +1,46 @@
+const tictactoe = require('../src/tictactoe');
+
+test('Board Creation', () => {
+  expect(tictactoe.getStringBoard([[1,2,3],[4,5,6],[7,8,9]])).toBe('1,2,3\n4,5,6\n7,8,9'); 
+});
+
+test('Win', () => {
+  expect(tictactoe.winnerMovementValidator('X',[['X','X','X'],['','',''],['','','']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['','',''],['X','X','X'],['','','']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['','',''],['','',''],['X','X','X']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['X','',''],['X','',''],['X','','']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['','X',''],['','X',''],['','X','']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['','','X'],['','','X'],['','','X']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['X','',''],['','X',''],['','','X']])).toBe(1);
+  expect(tictactoe.winnerMovementValidator('X',[['','','X'],['','X',''],['X','','']])).toBe(1);
+});
+
+test('Lose', () => {
+  expect(tictactoe.winnerMovementValidator('O',[['O','O','O'],['','',''],['','','']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['','',''],['O','O','O'],['','','']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['','',''],['','',''],['O','O','O']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['O','',''],['O','',''],['O','','']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['','O',''],['','O',''],['','O','']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['','','O'],['','','O'],['','','O']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['O','',''],['','O',''],['','','O']])).toBe(0);
+  expect(tictactoe.winnerMovementValidator('O',[['','','O'],['','O',''],['O','','']])).toBe(0);
+});
+
+test('Draw', () => {
+  expect(tictactoe.winnerMovementValidator('X',[['X','O','X'],['O','O','X'],['X','X','O']])).toBe(2);
+  expect(tictactoe.winnerMovementValidator('O',[['X','O','X'],['O','O','X'],['X','X','O']])).toBe(2);
+});
+
+test('Validate Input Number', () => {
+  expect(tictactoe.validateInputNumber(1)).toBe(true);
+  expect(tictactoe.validateInputNumber(2)).toBe(true);
+  expect(tictactoe.validateInputNumber(3)).toBe(true);
+  expect(tictactoe.validateInputNumber(4)).toBe(true);
+  expect(tictactoe.validateInputNumber(5)).toBe(true);
+  expect(tictactoe.validateInputNumber(6)).toBe(true);
+  expect(tictactoe.validateInputNumber(7)).toBe(true);
+  expect(tictactoe.validateInputNumber(8)).toBe(true);
+  expect(tictactoe.validateInputNumber(9)).toBe(true);
+  expect(tictactoe.validateInputNumber(10)).toBe(false);
+  expect(tictactoe.validateInputNumber('')).toBe(false);
+});
